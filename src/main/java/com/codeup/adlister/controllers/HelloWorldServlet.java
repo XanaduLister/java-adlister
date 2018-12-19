91ec35c10045f66280c1729cda1e6341dace9f1d
Julian Lundquist <julian.j.lundquist@gmail.com>
2018-12-19 17:00:38 -0600

    lots of spicey changes background color etc...

--- src/main/java/com/codeup/adlister/controllers/HelloWorldServlet.java
+++ src/main/java/com/codeup/adlister/controllers/HelloWorldServlet.java
@@ -9,18 +9,24 @@ import java.io.IOException;
 @WebServlet(name = "controllers.HelloWorldServlet", urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("<!doctype html>\n" +
+        response.getWriter().println(
+                "<!doctype html>\n" +
                 "<html lang=\"en\">\n" +
                 "<head>\n" +
-                "    <meta charset=\"utf-8\">\n" +
-                "    <title>Page Not Found</title>\n" +
+                "<link href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">" +
+                "<link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.6.1/css/all.css\" integrity=\"sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP\" crossorigin=\"anonymous\">" +
+                "<style>\n" +
+                "    body {\n" +
+                "        background-color: lightgrey;\n" +
+                "    }\n" +
+                "</style>" +
                 "</head>\n" +
                 "<body>\n" +
-                "<h1>Page Not Found</h1>\n" +
-                "<p>Sorry, but the page you were trying to view does not exist.</p>\n" +
+                "<h1 class=\"text-center customfont text-warning\">Page Not Found</h1>\n" +
+                "<p class=\"text-center\">Sorry, but the page you were trying to view does not exist.</p>\n" +
                 "<br>\n" +
                 "</body>\n" +
-                "<img src=\"https://rainbowwhiskers.files.wordpress.com/2012/01/rainbowwhiskers.png\">\n" +
+                "<div class=\"text-center\"><img src=\"https://rainbowwhiskers.files.wordpress.com/2012/01/rainbowwhiskers.png\"></div>\n" +
                 "</html>");
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,6 +5,11 @@
     <jsp:include page="/WEB-INF/partials/head.jsp">
         <jsp:param name="title" value="Welcome to my site!" />
     </jsp:include>
+    <style>
+        body {
+            background-color: lightgrey;
+        }
+    </style>
 </head>
 <body>
     <jsp:include page="/WEB-INF/partials/navbar.jsp" />
